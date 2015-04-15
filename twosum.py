993315c936235c4ff27159134a92d017145ce08f
ceparadise <yalin@brandeis.edu>
2015-04-15 02:26:22 -0400

    4/15/utilize

--- twosum.py
+++ twosum.py
@@ -2,14 +2,12 @@ class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
         dict={}
-        for i,x in enumerate(num):
-           dict[x]=i
-        for x in num:
-            y = target-x
-            if y in num:
-                i = dict[x]
-                j = dict[y]
-                return i, j
+        for i in xrange(len(num)):
+            y = target-num[i]
+            if y in dict:
+                return dict[y], i+1
+            dict[num[i]]= i+1
+
 if __name__ == '__main__':
     num = [2,3,4,0,1,5]
     target = 5
